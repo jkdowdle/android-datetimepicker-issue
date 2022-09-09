005d0e1f20214efb3b61ccd4f4231b9499a23868
josh-dowdle <joshua.dowdle@lifeomic.com>
2022-09-08 20:06:52 -0600

    reproduce issue

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Open"
+        onPress={() => {
+          DateTimePickerAndroid.open({
+            value: new Date(),
+            display: "spinner",
+          });
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
